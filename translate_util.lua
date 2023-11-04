1835dd6b076e72fdc29c0fbf9176d87800b24d45
Kyle McShea <46171436+kylemcshea@users.noreply.github.com>
2023-11-04 02:09:38 -0400

    create translate function to allow for dynamic values to be appended

---
+++ translate_util.lua
@@ -0,0 +1,24 @@
+local tObject = DREAM_TRANSLATE[Config.LANGUAGE]
+
+---@param key string
+---@param values table | nil
+function t(key, values)
+    if not tObject[key] then
+        print('Translation key not found: ' .. key)
+        return key
+    end
+
+    local text = tObject[key]
+
+    if (not values) then
+        return text
+    end
+
+    for tKey, value in pairs(values) do
+        print(tKey)
+        print(value)
+        text = text:gsub('{{ ' .. tKey .. ' }}', tostring(value))
+    end
+
+    return text
+end
